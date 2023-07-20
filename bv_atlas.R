0a5dc123a256367d48de862025986f802e920cad
josselinbarry <josselin.barry@ofb.gouv.fr>
2023-07-20 12:24:49 +0200

    again and again

--- bv_atlas.R
+++ bv_atlas.R
@@ -22,13 +22,28 @@ troncons_topage <-
   sf::read_sf(dsn = "data/TronconHydrographique_Bretagne_Pays_de_la_Loire_non_aqueduc_strahler.shp")
 
 bv_bretagne <- 
-  sf::read_sf(dsn = "data/bv_20230720_indicateurs.shp")
+  sf::read_sf(dsn = "data/bv_20230720_w_indicateurs.shp")
 
-# Ajouter la longueur des lignes
+# Ajouter la longueur des tronçons
 
 troncons_topage2 <- troncons_topage %>%
   mutate(longueur = st_length(troncons_topage))
 
+# Ajouter la surface des BV
+
+bv_bretagne2 <- bv_bretagne %>%
+  mutate(surface = st_area(bv_bretagne))
+
+# Ajouter des classes de BV
+
+bv_bretagne3 <- bv_bretagne2 %>%
+  mutate(classe_taille_bv = case_when(
+    (surface > 0 & surface <1000000) ~ "Moins de 100 ha", 
+    (surface > 1000000 & surface <5000000) ~ "Entre 100 et 1000 ha",
+    (surface > 5000000 & surface <10000000) ~ "Entre 500 et 1000 ha", 
+    (surface > 10000000 & surface <50000000) ~ "Entre 1000 et 5000 ha",
+    (surface > 50000000 & surface <100000000) ~ "Entre 5000 et 10000 ha",
+    (surface > 100000000) ~ "Plus de 10000 ha"))
 
 # Calcul des indicateurs
 
@@ -49,4 +64,18 @@ lineaire_rang <- troncons_topage2 %>%
   mutate(long_totale_km = long_totale_m/1000) %>%
   select(StreamOrde, long_totale_km)
 
-# Graph d'indicateurs            
+bv_median <- bv_bretagne2 %>%
+  sf::st_drop_geometry() %>% 
+  select(IDD, surface) %>%
+  as.data.frame() %>%
+  summarise(surface_mediane = median(surface))
+
+# Graph d'indicateurs          
+
+lineaire_rang %>% 
+  ggplot(aes(x = StreamOrde, y = long_totale_km)) +
+  geom_bar(fill = "blue") +
+  coord_flip() +
+  labs(x = "Rang de Strahler",
+       y = "Linéaire",
+       title = "Linéaire de réseau hydrographique par rang de Strahler")
