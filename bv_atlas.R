3e633e993fec360dab3b7bbbd8dc06b9f7e56daa
josselinbarry <josselin.barry@ofb.gouv.fr>
2023-07-20 14:26:49 +0200

    waou

--- bv_atlas.R
+++ bv_atlas.R
@@ -16,25 +16,28 @@ library(sf)
 
 source(file = "R/functions.R")
 
-# Chargement données
+# Chargement données----
 
 troncons_topage <- 
-  sf::read_sf(dsn = "data/TronconHydrographique_Bretagne_Pays_de_la_Loire_non_aqueduc_strahler.shp")
+  sf::read_sf(dsn = "data/TronconHydrographique_BV_cotiers_Bretagne_non_aqueduc_strahler.shp")
 
 bv_bretagne <- 
   sf::read_sf(dsn = "data/bv_20230720_w_indicateurs.shp")
 
-# Ajouter la longueur des tronçons
+# Compléments géographiques----
+
+## Ajouter la longueur des tronçons----
 
 troncons_topage2 <- troncons_topage %>%
   mutate(longueur = st_length(troncons_topage))
 
-# Ajouter la surface des BV
+## Ajouter la surface des BV----
 
 bv_bretagne2 <- bv_bretagne %>%
-  mutate(surface = st_area(bv_bretagne))
+  mutate(surface = st_area(bv_bretagne),
+         surface = as.numeric(surface))
 
-# Ajouter des classes de BV
+## Ajouter des classes de BV----
 
 bv_bretagne3 <- bv_bretagne2 %>%
   mutate(classe_taille_bv = case_when(
@@ -44,8 +47,10 @@ bv_bretagne3 <- bv_bretagne2 %>%
     (surface > 10000000 & surface <50000000) ~ "Entre 1000 et 5000 ha",
     (surface > 50000000 & surface <100000000) ~ "Entre 5000 et 10000 ha",
     (surface > 100000000) ~ "Plus de 10000 ha"))
+  
+# Calcul des indicateurs ----
 
-# Calcul des indicateurs
+## Linéaire total de réseau ----
 
 lineaire_total <- troncons_topage2 %>%
   sf::st_drop_geometry() %>% 
@@ -55,6 +60,8 @@ lineaire_total <- troncons_topage2 %>%
   mutate(longueur_totale_km = longueur_totale_m/1000) %>%
   select(longueur_totale_km)
 
+## Linéaire de réseau par rang de strahler ----
+
 lineaire_rang <- troncons_topage2 %>%
   sf::st_drop_geometry() %>% 
   select(CdOH, longueur, StreamOrde) %>%
@@ -63,6 +70,8 @@ lineaire_rang <- troncons_topage2 %>%
   summarise(long_totale_m = sum(longueur), na.rm = T) %>%
   mutate(long_totale_km = long_totale_m/1000) %>%
   select(StreamOrde, long_totale_km)
+
+## BV median
 
 bv_median <- bv_bretagne2 %>%
   sf::st_drop_geometry() %>% 
