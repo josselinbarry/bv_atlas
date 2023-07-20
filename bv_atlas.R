b1b7766bd1a6c8a8568ca7af24229a46b7aa9f94
josselinbarry <josselin.barry@ofb.gouv.fr>
2023-07-20 11:41:23 +0200

    hé hé

--- bv_atlas.R
+++ bv_atlas.R
@@ -19,17 +19,34 @@ source(file = "R/functions.R")
 # Chargement données
 
 troncons_topage <- 
-  sf::read_sf(dsn = "data/TronconHydrographique_BV_cotiers_Bretagne_non_aqueduc_strahler.shp")
+  sf::read_sf(dsn = "data/TronconHydrographique_Bretagne_Pays_de_la_Loire_non_aqueduc_strahler.shp")
 
 bv_bretagne <- 
   sf::read_sf(dsn = "data/bv_20230720_indicateurs.shp")
 
+# Ajouter la longueur des lignes
+
+troncons_topage2 <- troncons_topage %>%
+  mutate(longueur = st_length(troncons_topage))
+
+
 # Calcul des indicateurs
 
-lineaire_rang <- troncons_topage %>%
+lineaire_total <- troncons_topage2 %>%
   sf::st_drop_geometry() %>% 
-  select(cdOH, longueur, StreamOrder) %>%
+  select(CdOH, longueur) %>%
   as.data.frame() %>%
-  group_by(StreamOrder) %>%
-  summarise(long_totale = sum(longueur), na.rm = T)
-            
+  summarise(longueur_totale_m = sum(longueur)) %>%
+  mutate(longueur_totale_km = longueur_totale_m/1000) %>%
+  select(longueur_totale_km)
+
+lineaire_rang <- troncons_topage2 %>%
+  sf::st_drop_geometry() %>% 
+  select(CdOH, longueur, StreamOrde) %>%
+  as.data.frame() %>%
+  group_by(StreamOrde) %>%
+  summarise(long_totale_m = sum(longueur), na.rm = T) %>%
+  mutate(long_totale_km = long_totale_m/1000) %>%
+  select(StreamOrde, long_totale_km)
+
+# Graph d'indicateurs            
